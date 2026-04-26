a07e5537b965a784eccdb810bf4b3efc137c9725
Rajarupan Sampanthan <rajarupans@gmail.com>
2026-04-26 12:17:34 -0400

    Basic Template of MCP server

---
+++ README.md
@@ -0,0 +1,9 @@
+
+
+
+
+Config Steps
+
+
+Add your gemini api key as a envirionment variable:
+export GEMINI_API_KEY=<YOUR KEY>
--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from emotional-computer!")
+from fastmcp import Client
+from google import genai
+import asyncio
 
+mcp_client = Client("server.py")
+gemini_client = genai.Client()
+model_name = "gemini-3.1-flash-lite-preview"
+
+
+
+async def main():    
+    async with mcp_client:
+        response = await gemini_client.aio.models.generate_content(
+            model=model_name,
+            contents="Roll 3 dice!",
+            config=genai.types.GenerateContentConfig(
+                temperature=0,
+                tools=[mcp_client.session],  # Pass the FastMCP client session
+            ),
+        )
+        print(response.text)
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
